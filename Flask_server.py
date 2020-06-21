78ccf83d29a8dbb5dccf4207b94a3e330cb20664
Gauransh <gauransh30@gmail.com>
2020-06-21 20:31:02 +0530

    New files

--- Flask_server.py
+++ Flask_server.py
@@ -1,48 +1,24 @@
-from flask import *
-import tensorflow
-import keras
-import json
-import base64
-import numpy as np
-from keras.preprocessing.image import img_to_array
-import cv2
-import time
-from imageai.Detection import ObjectDetection
-import werkzeug
+from Functions import *
 
 app = Flask(__name__)
 
-detector = ObjectDetection()
+detector = None 
 
 @app.route('/image/', methods=['POST'])
 def image_request():
     if(request.method=='POST'):
 
         json_body= request.get_json()
-        imagefile = json_body.get('image')
-        objects=json_body.get('objects')
+
+        image_name, image_type, image_file, objects, minimum_probability, detection_speed = handle_request(json_body)
         
-        with open("imageToSave.png", "wb") as fh:
-            fh.write(base64.b64decode(imagefile))
+        image_path = upload_image(image_file, image_name, image_type)
 
         t0=time.perf_counter()
-        if len(objects)!=0:
-            custom = detector.CustomObjects()
-
-            for item in objects:
-                custom[item]='valid'
-
-            print('Custom')
-            detections = detector.detectCustomObjectsFromImage(custom_objects = custom, input_image= 'imageToSave.png', output_image_path= 'imageToSavetest.png',minimum_percentage_probability=35, thread_safe=True)
-        else :
-            print('Not custom')
-            detections = detector.detectObjectsFromImage(input_image= 'imageToSave.png', output_image_path= 'imageToSavetest.png',minimum_percentage_probability=35, thread_safe=True)
-
+        detections = Detection(detector, objects, image_path, minimum_probability)
         t1=time.perf_counter()
-        #detections = json.dumps(detections, default= int, skipkeys=True)
-
+        
         print("Time elapsed in detection:", t1-t0)
-        list = [0,0,0,0], [1,1,1,1]
 
         return jsonify(
             objects = json.dumps(detections, default= int)
@@ -53,13 +29,11 @@ def hello():
     return "Hello"
 
 if __name__ == '__main__':
-    t0=time.perf_counter()
 
-    detector.setModelTypeAsYOLOv3()
-    detector.setModelPath('yolo.h5')
-    detector.loadModel(detection_speed='fastest')
-    
-    
+    t0=time.perf_counter()
+    detector = load_model() 
     t1=time.perf_counter()
+
     print("Time elapsed in loading model:", t1-t0)
-    app.run(debug=True)
+
+    app.run(debug=True, host= json_config['host'], port=json_config['port'])
---
+++ Functions.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify, abort, Response
+import tensorflow
+import keras
+import json
+import base64
+import time
+from imageai.Detection import ObjectDetection
+
+json_config = json.load(open('config.json'))
+
+def load_model(model_path = json_config['model_file']['default'], detection_speed = json_config['default_parameters']['default_speed']):
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(model_path)
+    detector.loadModel(detection_speed=detection_speed)
+
+    return detector
+
+def Detection(detector, objects, image_path, minimum_probability = json_config['default_parameters']['default_probability']):
+    input_dir = json_config['default_parameters']['upload_directory']
+    output_dir = json_config['default_parameters']['detected_directory']
+
+    input_path = input_dir + image_path
+    output_path = output_dir + image_path
+
+    if len(objects)!=0:
+        custom = detector.CustomObjects() 
+
+        for item in objects:
+            custom[item]='valid'
+
+        try :
+            detections = detector.detectCustomObjectsFromImage(custom_objects = custom, input_image= input_path, output_image_path= output_path,minimum_percentage_probability=minimum_probability, thread_safe=True)
+        except :
+            abort(Response('Invalid image format', status= 400))
+    else :
+
+        try:
+            detections = detector.detectObjectsFromImage(input_image= input_path, output_image_path= output_path,minimum_percentage_probability=minimum_probability, thread_safe=True)
+        except :
+            abort(Response('Invalid image format', status= 400))  
+
+    return detections
+
+def upload_image(imagefile, image_name, image_type):
+    image_path = image_name + str(time.time()) + "." + image_type
+    try:
+        with open(json_config['default_parameters']['upload_directory'] + image_path, "wb") as fh:
+            fh.write(base64.b64decode(imagefile))
+    except :
+        abort(Response('Invalid image', status= 400))
+    return image_path
+
+# def delete_image(input_path):
+#     with open(json_config['upload_directory'] + image_path, "wb") as
+
+def handle_request(json_body):
+    image_file = json_body.get('image')
+    image_name = json_body.get('image_name')
+    image_type = json_body.get('image_type')
+    objects = json_body.get('objects')
+    minimum_probability = json_body.get('minimum_probability') if json_body.get('minimum_probability')!=None else json_config['default_parameters']['default_probability']
+    detection_speed = json_body.get('detection_speed') if json_body.get('detection_speed')!=None else json_config['default_parameters']['default_speed']
+
+    if not isinstance(image_file, str) or not isinstance(image_name, str) or not isinstance(image_type, str) or not isinstance(objects, list) or not isinstance(minimum_probability, int) and not isinstance(minimum_probability, float) or not isinstance(detection_speed, str) :
+        abort(400)
+
+    return image_name, image_type, image_file, objects, minimum_probability, detection_speed
+    
