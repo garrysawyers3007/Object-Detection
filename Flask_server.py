94a2630c54193c501dd0f033f5af0849f6214746
Gauransh <gauransh30@gmail.com>
2020-06-22 22:11:19 +0530

    --

--- Flask_server.py
+++ Flask_server.py
@@ -3,6 +3,8 @@ from Functions import *
 app = Flask(__name__)
 
 detector = None 
+json_config = None
+logger(app)
 
 @app.route('/image/', methods=['POST'])
 def image_request():
@@ -10,12 +12,12 @@ def image_request():
 
         json_body= request.get_json()
 
-        image_name, image_type, image_file, objects, minimum_probability, detection_speed = handle_request(json_body)
+        image_name, image_type, image_file, objects, minimum_probability, detection_speed = handle_request(json_body, json_config)
         
-        image_path = upload_image(image_file, image_name, image_type)
+        image_path = upload_image(json_config, image_file, image_name, image_type)
 
         t0=time.perf_counter()
-        detections = Detection(detector, objects, image_path, minimum_probability)
+        detections = Detection(json_config, detector, objects, image_path, minimum_probability)
         t1=time.perf_counter()
         
         print("Time elapsed in detection:", t1-t0)
@@ -30,8 +32,16 @@ def hello():
 
 if __name__ == '__main__':
 
+    json_config = get_config('config.json')
+
+    #logger(json_config, app)
+    upload_path = json_config['default_parameters']['upload_directory']
+    detected_path = json_config['default_parameters']['detected_directory']
+
+    check_dirs([upload_path, detected_path])
+
     t0=time.perf_counter()
-    detector = load_model() 
+    detector = load_model(json_config['model_file']['default'], json_config['default_parameters']['default_speed']) 
     t1=time.perf_counter()
 
     print("Time elapsed in loading model:", t1-t0)
--- Functions.py
+++ Functions.py
@@ -1,15 +1,28 @@
 from flask import Flask, request, jsonify, abort, Response
+from logging import FileHandler, ERROR, Formatter
 import tensorflow
 import keras
 import json
 import base64
 import time
+from datetime import date, datetime
 import os
 from imageai.Detection import ObjectDetection
 
-json_config = json.load(open('config.json'))
 
-def load_model(model_path = json_config['model_file']['default'], detection_speed = json_config['default_parameters']['default_speed']):
+def check_dirs(paths):
+    for path in paths:
+        if(not os.path.isdir(path)):
+            os.mkdir(path)
+    
+def get_config(file_name):
+    try:
+        json_config = json.load(open(file_name))
+        return json_config
+    except FileNotFoundError as e:
+        abort(500)
+
+def load_model(model_path , detection_speed):
     detector = ObjectDetection()
     detector.setModelTypeAsYOLOv3()
     detector.setModelPath(model_path)
@@ -17,9 +30,16 @@ def load_model(model_path = json_config['model_file']['default'], detection_spee
 
     return detector
 
-def Detection(detector, objects, image_path, minimum_probability = json_config['default_parameters']['default_probability']):
-    input_dir = json_config['default_parameters']['upload_directory']
-    output_dir = json_config['default_parameters']['detected_directory']
+def Detection(json_config, detector, objects, image_path, minimum_probability):
+    curr_date = str(date.today())
+    curr_time = str(datetime.now().strftime("%H-%M-%S"))
+
+    detected_dir = json_config['default_parameters']['detected_directory']
+    upload_dir = json_config['default_parameters']['upload_directory']
+    
+    input_dir = upload_dir + curr_date + '/'
+    output_dir =  detected_dir + curr_date + '/'
+    check_dirs([detected_dir, input_dir, output_dir])
 
     input_path = input_dir + image_path
     output_path = output_dir + image_path
@@ -32,25 +52,34 @@ def Detection(detector, objects, image_path, minimum_probability = json_config['
 
         try :
             detections = detector.detectCustomObjectsFromImage(custom_objects = custom, input_image= input_path, output_image_path= output_path,minimum_percentage_probability=minimum_probability, thread_safe=True)
-        except :
-            abort(Response('Invalid image format', status= 400))
+        except Exception as e:
+            abort(Response('Error - {}'.format(e), status= 400))
     else :
 
         try:
             detections = detector.detectObjectsFromImage(input_image= input_path, output_image_path= output_path,minimum_percentage_probability=minimum_probability, thread_safe=True)
-        except :
-            abort(Response('Invalid image format', status= 400))  
+        except Exception as e:
+            abort(Response('Error - {}'.format(e), status= 400))  
 
     #delete_image(input_path)
     return detections
 
-def upload_image(imagefile, image_name, image_type):
-    image_path = image_name + str(time.time()) + "." + image_type
+def upload_image(json_config, image_file, image_name, image_type):
+
+    curr_date = str(date.today())
+    curr_time = str(datetime.now().strftime("%H-%M-%S"))
+
+    uploaded_dir = json_config['default_parameters']['upload_directory']
+    input_dir = uploaded_dir + curr_date + '/'
+    check_dirs([uploaded_dir, input_dir])
+    image_path = image_name + '_' + curr_date + '_' + curr_time + "." + image_type
+
     try:
-        with open(json_config['default_parameters']['upload_directory'] + image_path, "wb") as fh:
-            fh.write(base64.b64decode(imagefile))
-    except :
-        abort(Response('Invalid image', status= 400))
+        with open(input_dir + image_path, "wb") as fh:
+            fh.write(base64.b64decode(image_file))
+    except Exception as e:
+        
+        abort(Response('Error - {}'.format(e), status= 400))
     return image_path
 
 def delete_image(input_path):
@@ -58,7 +87,7 @@ def delete_image(input_path):
         os.remove(input_path)
          
 
-def handle_request(json_body):
+def handle_request(json_body, json_config):
     image_file = json_body.get('image')
     image_name = json_body.get('image_name')
     image_type = json_body.get('image_type')
@@ -68,7 +97,15 @@ def handle_request(json_body):
     detection_speed = json_body.get('detection_speed') if json_body.get('detection_speed')!=None else json_config['default_parameters']['default_speed']
 
     if not isinstance(image_file, str) or not isinstance(image_name, str) or not isinstance(image_type, str) or not isinstance(objects, list) or not isinstance(minimum_probability, int) and not isinstance(minimum_probability, float) or not isinstance(detection_speed, str) :
-        abort(400)
+        abort(Response('Error - Invalid Request', status= 400))
 
     return image_name, image_type, image_file, objects, minimum_probability, detection_speed
     
+def logger(app):
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(Formatter(
+        ''' 
+        %(asctime)s - %(message)s
+        '''
+    ))
+    app.logger.addHandler(file_handler)
