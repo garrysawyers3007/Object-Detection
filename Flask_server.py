71747973a60faec127375ae8155dde0d1a8d8614
Gauransh <gauransh30@gmail.com>
2020-06-23 18:51:22 +0530

    Comments added

--- Flask_server.py
+++ Flask_server.py
@@ -9,20 +9,24 @@ json_config = None
 def image_request():
     if(request.method=='POST'):
 
-        json_body= request.get_json()
+        json_body= request.get_json() #get json from request
 
+        #get data from the request json
         objects, minimum_probability, detection_speed, unique_id = handle_user_request(json_body, json_config)
 
         image_name, image_type, image_file = handle_image_request(json_body, unique_id)
 
+        #upload the retrieved image to the uploads directory 
         image_path = upload_image(json_config, image_file, image_name, image_type, unique_id)
 
+        # detect the objects in the image as per user input
         t0=time.perf_counter()
         detections = Detection(json_config, detector, objects, image_path, minimum_probability, unique_id)
         t1=time.perf_counter()
         
         print("Time elapsed in detection:", t1-t0)
 
+        #return the response
         return jsonify(
             objects = json.dumps(detections, default= int)
         )
@@ -37,9 +41,10 @@ if __name__ == '__main__':
 
     upload_path = json_config['default_parameters']['upload_directory']
     detected_path = json_config['default_parameters']['detected_directory']
-
+    #checking if directories for uploaded and detected images exist if not create them
     check_dirs([upload_path, detected_path])
 
+    #loading the model before start of server
     t0=time.perf_counter()
     detector = load_model(json_config['model_file']['default'], json_config['default_parameters']['default_speed']) 
     t1=time.perf_counter()
